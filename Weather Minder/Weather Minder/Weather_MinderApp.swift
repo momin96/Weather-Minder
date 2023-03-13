205a0f5aed19d9d2576779a0c0b53177d558fad6
Nasir <momin96@gmail.com>
2023-03-13 15:04:09 +0530

    Initial Project created

---
+++ Weather Minder/Weather Minder/Weather_MinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_MinderApp.swift
+//  Weather Minder
+//
+//  Created by Nasir Ahmed Momin on 13/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_MinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
