b7a39da97cc177b3d1a707d3d970f4249c333fc5
Nasir <momin96@gmail.com>
2023-03-13 18:38:03 +0530

    Searching city names

--- Weather Minder/Weather Minder/ContentView.swift
+++ Weather Minder/Weather Minder/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = ContentViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+            }
+            .searchable(text: $viewModel.searchText,
+                        prompt: Text("Enter cities name by comma separated"))
+            .onChange(of: viewModel.searchText) { newValue in
+                viewModel.performSearch(with: newValue)
+            }
         }
-        .padding()
+        .navigationTitle("Searchable List")
+    }
+}
+
+class ContentViewModel: ObservableObject {
+    @Published var searchText = ""
+    
+    func performSearch(with text: String) {
+        print(text)
+        if text.count >= 3 {
+            // Make call
+        }
     }
 }
 
