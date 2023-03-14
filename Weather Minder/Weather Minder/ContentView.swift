0c838ba1266b1a710181c6b15bbdb6a8cba40388
Nasir <momin96@gmail.com>
2023-03-14 09:21:01 +0530

    Implementation of getting weather

--- Weather Minder/Weather Minder/ContentView.swift
+++ Weather Minder/Weather Minder/ContentView.swift
@@ -21,26 +21,101 @@ struct ContentView: View {
             .searchable(text: $viewModel.searchText,
                         prompt: Text("Enter cities name by comma separated"))
             .onChange(of: viewModel.searchText) { newValue in
-                viewModel.performSearch(with: newValue)
+                viewModel.shouldEnabledSearch(from: newValue)
             }
+            .onSubmit(of: .search) {
+                viewModel.performSearch(with: viewModel.searchText)
+            }
+            .disableAutocorrection(viewModel.disableSearchButton)
         }
         .navigationTitle("Searchable List")
     }
 }
 
 class ContentViewModel: ObservableObject {
-    @Published var searchText = ""
+    @Published var searchText = "Mumbai, New York, Berlin"
+    @Published var disableSearchButton = false
+    
+    func shouldEnabledSearch(from text: String) {
+        let cities = text.components(separatedBy: ",")
+        if cities.count < 3 || cities.count > 7 {
+            disableSearchButton = true
+        } else {
+            disableSearchButton = false
+        }
+    }
     
     func performSearch(with text: String) {
-        print(text)
-        if text.count >= 3 {
-            // Make call
+        let cities = text.components(separatedBy: ",")
+        for city in cities {
+            Task {
+                do {
+                    let coordinates = try await GeoCoderUseCase().geocode(with: city)
+                    getWeather(for: city, and: coordinates)
+                } catch {
+                    print(error)
+                }
+            }
         }
+    }
+    
+    func getWeather(for city: String, and coordinates: CLLocationCoordinate2D) {
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+import CoreLocation
+
+struct GeoCoderUseCase {
+    
+    let geocoder = CLGeocoder()
+
+//
+//    func geo(_ cities: [String]) async -> any AsyncSequence {
+//        for city in cities {
+//            Task {
+//                do {
+//                    let location = try await GeoCoderUseCase().geocode(with: city)
+//                } catch {
+//
+//                }
+//            }
+//        }
+//    }
+//
+//
+//    func geocode(_ cities: [String]) async -> AsyncStream<Int> {
+//        return AsyncStream { continuation in
+//            for city in cities {
+//                do {
+//                    let location = try await GeoCoderUseCase().geocode(with: city)
+//                    continuation.yield(location)
+//                } catch {
+//                    continuation.finish(throwing: error)
+//                }
+//            }
+//            continuation.finish()
+//        }
+//    }
+    
+    func geocode(with city: String) async throws -> CLLocationCoordinate2D {
+        let geocoder = CLGeocoder()
+        let placemarks = try await geocoder.geocodeAddressString(city)
+        guard let placemark = placemarks.first,
+              let location = placemark.location else {
+            throw NSError(domain: "com.example.geocoding",
+                          code: 1,
+                          userInfo: [NSLocalizedDescriptionKey: "Unable to geocode city"])
+            
+        }
+        
+        return location.coordinate
     }
 }
